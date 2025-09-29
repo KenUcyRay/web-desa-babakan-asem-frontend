85da00b50b6121054155a177dfa462628844a6ea
radewan <ranggadendi6@gmail.com>
2025-09-29 10:33:13 +0700

    add credentials option to login request

--- src/libs/api/UserApi.js
+++ src/libs/api/UserApi.js
@@ -38,6 +38,7 @@ export class UserApi {
   static async login(body, language) {
     return await fetch(`${import.meta.env.VITE_NEW_BASE_URL}/users/login`, {
       method: "POST",
+      credentials: true,
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
